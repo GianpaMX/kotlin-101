0311e379e86bef08a6121cfc66724939ad9564b4
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2019-10-02 08:04:21 -0500

    Multiline

--- src/main/java/io/github/gianpamx/kotlin101/marvel/MarvelThumbnail.kt
+++ src/main/java/io/github/gianpamx/kotlin101/marvel/MarvelThumbnail.kt
@@ -3,4 +3,7 @@ package io.github.gianpamx.kotlin101.marvel
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class MarvelThumbnail constructor(val path: String? = null, val extension: String? = null)
+data class MarvelThumbnail(
+        val path: String? = null,
+        val extension: String? = null
+)
